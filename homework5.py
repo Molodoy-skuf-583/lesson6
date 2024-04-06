f0419a7b03093b824e4eaeb0adc79cdb66c076c0
Ivan <165202411+Molodoy-skuf-583@users.noreply.github.com>
2024-04-06 19:07:29 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,16 @@
+my_list = ['car', 'truck', 'motorcycle', 'bus']
+print(my_list)
+print(my_list[0:4:3])
+my_list = [('car', 'truck'), 'motorcycle', 'bus']
+print(my_list[0][0:5])#Если честно, не совсем понял...
+#этот этап задания, например я сделал подсписок...
+#но в нем только два элемента, получается мне...
+#изначально нужно было добавить в список больше элементов?
+my_list[1] = 'airplane'
+print(my_list)
+my_dict = {'police': 102, 'ambulance': 103, 'fire departament': 101, 'gas service': 104}
+print(my_dict)
+print(my_dict['police'])
+my_dict['police'] = 1000
+my_dict['Hello'] = 500
+print(my_dict)
